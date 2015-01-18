63f736330d9411595463830ef82cf25b27a0f4bd
Florian Wolters <wolters.fl@gmail.com>
2015-01-18 14:28:21 +0100

    Added build of test and example files.
    Signed-off-by: Florian Wolters <wolters.fl@gmail.com>

--- Bootstrap.cmake
+++ Bootstrap.cmake
@@ -10,22 +10,26 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-# Start of include guard.
+# ------------------------------------------------------------------------------
+# Include guard.
+# ------------------------------------------------------------------------------
+
 if(florianwolters_include_included)
   return()
 endif()
 
-set(florianwolters_include_included TRUE)
-# End of include guard.
+set(florianwolters_include_included 1)
 
 # ------------------------------------------------------------------------------
-# Include official "CMake" modules.
+# Load and run "CMake" code from a file or module.
 # ------------------------------------------------------------------------------
+
 include(ExternalProject)
 
 # ------------------------------------------------------------------------------
 # Include project specific "CMake" modules.
 # ------------------------------------------------------------------------------
+
 include("${CMAKE_CURRENT_LIST_DIR}/SetCommonVariables.cmake")
 include("${PROJECT_CMAKE_INCLUDES_SOURCE_DIR}/CMake.cmake")
 include("${PROJECT_CMAKE_INCLUDES_SOURCE_DIR}/Cppcheck.cmake")
@@ -35,3 +39,98 @@ include("${PROJECT_CMAKE_INCLUDES_SOURCE_DIR}/GoogleCpplint.cmake")
 include("${PROJECT_CMAKE_INCLUDES_SOURCE_DIR}/GoogleTest.cmake")
 
 assert_out_of_source_build()
+
+# ------------------------------------------------------------------------------
+# Provide options that the user can optionally select.
+# ------------------------------------------------------------------------------
+
+option(BUILD_TESTS "Build the test executables of the project." ON)
+option(BUILD_EXAMPLES "Build the example executables of the project." ON)
+
+# ------------------------------------------------------------------------------
+# Initialize project.
+# ------------------------------------------------------------------------------
+
+
+set(PROJECT_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}")
+
+project(${PROJECT_NAME} VERSION ${PROJECT_VERSION} LANGUAGES CXX C)
+
+# ------------------------------------------------------------------------------
+# Build tests.
+# ------------------------------------------------------------------------------
+
+if(BUILD_TESTS)
+  set(PROJECT_DEPENDENCY_TEST_LIBRARIES ${PROJECT_DEPENDENCY_LIBRARIES})
+
+  find_package(Threads REQUIRED)
+  list(APPEND PROJECT_DEPENDENCY_TEST_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
+
+  # Find "Google Test (GTest)".
+  find_package(GTest ${PROJECT_DEPENDENCY_GTEST_VERSION})
+  list(APPEND PROJECT_DEPENDENCY_TEST_LIBRARIES ${GTEST_BOTH_LIBRARIES})
+
+  if(NOT ${GTEST_FOUND})
+    get_googletest(${PROJECT_DEPENDENCY_GTEST_VERSION})
+  endif()
+
+  # "Google Test" is available now.
+  enable_testing()
+
+  include_directories(SYSTEM ${GTEST_INCLUDE_DIRS})
+
+  foreach(TEST_SOURCE_FILE ${PROJECT_TEST_DEFINITION_SOURCE_FILES_LIST})
+    get_filename_component(TEST_NAME ${TEST_SOURCE_FILE} NAME_WE)
+    add_executable(${TEST_NAME} ${TEST_SOURCE_FILE})
+    target_include_directories(${TEST_NAME} PRIVATE
+                               ${PROJECT_DECLARATION_SOURCE_DIR}
+                               ${PROJECT_TEST_SOURCE_DIR})
+    target_link_libraries(${TEST_NAME} ${PROJECT_DEPENDENCY_TEST_LIBRARIES})
+    set_target_properties(${TEST_NAME}
+                          PROPERTIES CXX_STANDARD 11
+                                     CXX_STANDARD_REQUIRED ON)
+
+    set(REQUIRED_COMPILE_FEATURES
+        cxx_defaulted_functions
+        cxx_deleted_functions)
+
+    if(!WIN32)
+        target_compile_features(${TEST_NAME}
+                                PRIVATE ${REQUIRED_COMPILE_FEATURES})
+    else()
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+    endif()
+
+    add_test(NAME ${TEST_NAME} COMMAND ${TEST_NAME})
+  endforeach()
+endif()
+
+# ------------------------------------------------------------------------------
+# Build examples.
+# ------------------------------------------------------------------------------
+
+if(BUILD_EXAMPLES)
+  # TODO(wolters): Eliminate Copy & Paste with test creation.
+  foreach(EXAMPLE_SOURCE_FILE ${PROJECT_EXAMPLE_DEFINITION_SOURCE_FILES_LIST})
+    get_filename_component(EXAMPLE_NAME ${EXAMPLE_SOURCE_FILE} NAME_WE)
+    add_executable(${EXAMPLE_NAME} ${EXAMPLE_SOURCE_FILE})
+    target_include_directories(${EXAMPLE_NAME} PRIVATE
+                               ${PROJECT_DECLARATION_SOURCE_DIR})
+    target_link_libraries(${EXAMPLE_NAME}
+                          ${PROJECT_DEPENDENCY_LIBRARIES})
+    set_target_properties(${PROJECT_TARGET_EXAMPLE_EXECUTABLE}
+                          PROPERTIES CXX_STANDARD 11
+                                     CXX_STANDARD_REQUIRED ON)
+
+    set(REQUIRED_COMPILE_FEATURES
+        cxx_defaulted_functions
+        cxx_deleted_functions)
+
+    if(!WIN32)
+        target_compile_features(${EXAMPLE_NAME}
+                                PRIVATE ${REQUIRED_COMPILE_FEATURES})
+    else()
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+    endif()
+  endforeach()
+endif()
